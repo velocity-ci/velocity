a06f955051502b0134c59b279f5814ec416f7e1d
Eddy Lane <naedin@gmail.com>
2017-09-18 23:10:58 +0100

    [web+api] Mapped server side project errors back to client form fields

--- web/velocity/src/app/Views/Form.elm
+++ web/velocity/src/app/Views/Form.elm
@@ -4,19 +4,37 @@ import Html exposing (fieldset, ul, li, Html, Attribute, text, div, label, span,
 import Html.Attributes exposing (class, type_, for, id)
 
 
-password : String -> String -> List (Attribute msg) -> List (Html msg) -> Html msg
-password name labelText attrs =
-    control name labelText Html.input ([ type_ "password" ] ++ attrs)
+password :
+    String
+    -> String
+    -> List ( field, String )
+    -> List (Attribute msg)
+    -> List (Html msg)
+    -> Html msg
+password name labelText errors attrs =
+    control name labelText errors Html.input ([ type_ "password" ] ++ attrs)
 
 
-input : String -> String -> List (Attribute msg) -> List (Html msg) -> Html msg
-input name labelText attrs =
-    control name labelText Html.input ([ type_ "text" ] ++ attrs)
+input :
+    String
+    -> String
+    -> List ( field, String )
+    -> List (Attribute msg)
+    -> List (Html msg)
+    -> Html msg
+input name labelText errors attrs =
+    control name labelText errors Html.input ([ type_ "text" ] ++ attrs)
 
 
-textarea : String -> String -> List (Attribute msg) -> List (Html msg) -> Html msg
-textarea name labelText =
-    control name labelText Html.textarea
+textarea :
+    String
+    -> String
+    -> List ( field, String )
+    -> List (Attribute msg)
+    -> List (Html msg)
+    -> Html msg
+textarea name labelText errors attrs =
+    control name labelText errors Html.textarea attrs
 
 
 viewErrors : List ( a, String ) -> Html msg
@@ -38,15 +56,23 @@ viewSpinner =
 -- INTERNAL --
 
 
+viewErrorFeedback : ( field, String ) -> Html msg
+viewErrorFeedback error =
+    div [ class "invalid-feedback" ] [ text (Tuple.second error) ]
+
+
 control :
     String
     -> String
+    -> List ( field, String )
     -> (List (Attribute msg) -> List (Html msg) -> Html msg)
     -> List (Attribute msg)
     -> List (Html msg)
     -> Html msg
-control name labelText element attributes children =
+control name labelText errors element attributes children =
     div [ class "form-group" ]
-        [ label [ for name ] [ text labelText ]
-        , element (class "form-control" :: id name :: attributes) children
-        ]
+        ([ label [ for name ] [ text labelText ]
+         , element (class "form-control" :: id name :: attributes) children
+         ]
+            ++ List.map viewErrorFeedback errors
+        )
