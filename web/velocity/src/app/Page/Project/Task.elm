6fe1586265eac75cbcfd2227b37e78a72123b1fd
Eddy Lane <naedin@gmail.com>
2017-10-02 00:59:52 +0100

    [web] Updated task page so that steps are in a collpasey accordion thing, ready for the build form to be placed at the top of page

--- web/velocity/src/app/Page/Project/Task.elm
+++ web/velocity/src/app/Page/Project/Task.elm
@@ -2,6 +2,7 @@ module Page.Project.Task exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import Data.Commit as Commit exposing (Commit)
 import Data.Session as Session exposing (Session)
 import Data.Project as Project exposing (Project)
@@ -23,6 +24,7 @@ import Page.Project.Commit as Commit
 type alias Model =
     { commit : Commit
     , task : ProjectTask.Task
+    , toggledStep : Maybe Step
     }
 
 
@@ -44,8 +46,19 @@ init session id hash name =
 
         handleLoadError _ =
             pageLoadError Page.Project "Project unavailable."
+
+        initialModel commit task =
+            let
+                toggledStep =
+                    task.steps
+                        |> List.head
+            in
+                { commit = commit
+                , task = task
+                , toggledStep = toggledStep
+                }
     in
-        Task.map2 Model loadCommit loadTask
+        Task.map2 initialModel loadCommit loadTask
             |> Task.mapError handleLoadError
 
 
@@ -55,30 +68,42 @@ init session id hash name =
 
 view : Model -> Html Msg
 view model =
-    viewStepList model.task.steps
+    viewStepList model.task.steps model.toggledStep
         |> div []
 
 
-viewStepList : List Step -> List (Html Msg)
-viewStepList steps =
+viewStepList : List Step -> Maybe Step -> List (Html Msg)
+viewStepList steps toggledStep =
     let
         stepView i step =
             let
                 stepNum =
                     i + 1
-            in
-                case step of
-                    Run runStep ->
-                        viewRunStep stepNum runStep
 
-                    Build buildStep ->
-                        viewBuildStep stepNum buildStep
+                runStep =
+                    viewRunStep stepNum
+
+                buildStep =
+                    viewBuildStep stepNum
+            in
+                case ( step, toggledStep ) of
+                    ( Run run, Just (Run toggled) ) ->
+                        runStep run (run == toggled)
+
+                    ( Build build, Just (Build toggled) ) ->
+                        buildStep build (build == toggled)
+
+                    ( Run run, _ ) ->
+                        runStep run False
+
+                    ( Build build, _ ) ->
+                        buildStep build False
     in
         List.indexedMap stepView steps
 
 
-viewBuildStep : Int -> BuildStep -> Html Msg
-viewBuildStep i buildStep =
+viewBuildStep : Int -> BuildStep -> Bool -> Html Msg
+viewBuildStep i buildStep toggled =
     let
         tagList =
             List.map (\t -> li [] [ text t ]) buildStep.tags
@@ -97,23 +122,20 @@ viewBuildStep i buildStep =
                 , dt [] [ text "Dockerfile" ]
                 , dd [] [ text buildStep.dockerfile ]
                 ]
+
+        title =
+            toString i ++ ". " ++ buildStep.description
     in
-        div [ class "card default-margin-bottom" ]
-            [ div [ class "card-header" ]
-                [ h5 [ class "mb-0" ]
-                    [ text (toString i ++ ". " ++ buildStep.description) ]
-                ]
-            , div [ class "card-body" ]
-                [ div [ class "row" ]
-                    [ div [ class "col-md-6" ] [ leftDl ]
-                    , div [ class "col-md-6" ] [ rightDl ]
-                    ]
+        viewStepCollapse (Build buildStep) title toggled <|
+            [ div [ class "row" ]
+                [ div [ class "col-md-6" ] [ leftDl ]
+                , div [ class "col-md-6" ] [ rightDl ]
                 ]
             ]
 
 
-viewRunStep : Int -> RunStep -> Html Msg
-viewRunStep i runStep =
+viewRunStep : Int -> RunStep -> Bool -> Html Msg
+viewRunStep i runStep toggled =
     let
         command =
             String.join " " runStep.command
@@ -150,17 +172,47 @@ viewRunStep i runStep =
                 , dt [] [ text "Command" ]
                 , dd [] [ text command ]
                 ]
+
+        title =
+            toString i ++ ". " ++ runStep.description
     in
-        div [ class "card default-margin-bottom" ]
-            [ div [ class "card-header" ]
-                [ h5 [ class "mb-0" ]
-                    [ text (toString i ++ ". " ++ runStep.description) ]
+        viewStepCollapse (Run runStep) title toggled <|
+            [ div [ class "row" ]
+                [ div [ class "col-md-6" ] [ leftDl ]
+                , div [ class "col-md-6" ] [ envTable ]
                 ]
-            , div [ class "card-body" ]
-                [ div [ class "row" ]
-                    [ div [ class "col-md-6" ] [ leftDl ]
-                    , div [ class "col-md-6" ] [ envTable ]
+            ]
+
+
+viewStepCollapse : Step -> String -> Bool -> List (Html Msg) -> Html Msg
+viewStepCollapse step title toggled contents =
+    let
+        msg =
+            if toggled then
+                ToggleStep Nothing
+            else
+                ToggleStep (Just step)
+
+        caretClassList =
+            [ ( "fa-caret-square-o-down", toggled )
+            , ( "fa-caret-square-o-up", not toggled )
+            ]
+    in
+        div [ class "card" ]
+            [ div [ class "card-header d-flex justify-content-between align-items-center", onClick msg ]
+                [ h5 [ class "mb-0" ] [ text title ]
+                , button
+                    [ type_ "button"
+                    , class "btn"
                     ]
+                    [ i [ class "fa", classList caretClassList ] []
+                    ]
+                ]
+            , div
+                [ class "collapse"
+                , classList [ ( "show", toggled ) ]
+                ]
+                [ div [ class "card-body" ] contents
                 ]
             ]
 
@@ -178,9 +230,12 @@ breadcrumb project commit task =
 
 
 type Msg
-    = NoOp
+    = ToggleStep (Maybe Step)
 
 
 update : Project -> Session -> Msg -> Model -> ( Model, Cmd Msg )
 update project session msg model =
-    model => Cmd.none
+    case msg of
+        ToggleStep maybeStep ->
+            { model | toggledStep = maybeStep }
+                => Cmd.none
