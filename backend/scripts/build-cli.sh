036b9f5aee8a4e7abee38b14e1f8ef57085dc51d
VJ Patel <vj@vjpatel.me>
2018-02-22 20:27:01 +0000

    [backend] Renamed vci-cli to vcli. Working builds for Velocity components

--- backend/scripts/build-cli.sh
+++ backend/scripts/build-cli.sh
@@ -6,4 +6,4 @@ go get -u github.com/mitchellh/gox
 
 gox -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}" \
     -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm" \
-    ./cmd/vci-cli
+    ./cmd/vcli
