cf105c77d026504745869fc2a236c76ea569f17c
VJ Patel <vj@vjpatel.me>
2018-04-06 01:57:47 +0100

    [backend] Added swagger support. Need to branch and add swagger defs with dredd

--- backend/scripts/ensure-dep.sh
+++ backend/scripts/ensure-dep.sh
@@ -3,9 +3,6 @@
 # Golang dep should hopefully be merged in to Go by 1.10 (https://github.com/golang/dep/wiki/Roadmap)
 if ! [ -x "$(command -v dep)" ]; then
     echo "dep is not installed. Installing..."
-    dep_version=0.4.1
-    curl -fsSL -o /usr/local/bin/dep \
-    https://github.com/golang/dep/releases/download/v${dep_version}/dep-linux-amd64 && \
-    chmod +x /usr/local/bin/dep
+    curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 fi
 echo "dep is installed."
