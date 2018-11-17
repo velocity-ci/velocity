4ff844dec68cd92591fe51e518fff92756d0b111
VJ Patel <vj@vjpatel.me>
2018-11-17 16:04:07 +0000

    update install script

--- backend/deployments/vcli/install.sh
+++ backend/deployments/vcli/install.sh
@@ -8,22 +8,28 @@ else
     echo "-> unsupported OS: ${OSTYPE}"
     exit 1
 fi
-
 echo "-> determined ${arch} OS"
+
+install_path="${HOME}/.local/bin"
+if [ $(whoami) == "root" ]; then
+    install_path="/usr/local/bin"
+fi
+echo "-> installing under ${install_path}"
+mkdir -p ${install_path}
+
+bin_path="${install_path}/vcli"
 repo="velocity-ci/velocity"
 download_url=$(curl -s https://api.github.com/repos/${repo}/releases/latest \
   | grep browser_download_url \
   | grep ${arch} \
   | cut -d '"' -f 4)
-
-bin_folder="${HOME}/.local/bin"
-
-mkdir -p ${bin_folder}
-bin_path="${bin_folder}/vcli"
-echo "-> downloading ${download_url}"
+echo "-> downloading ${download_url} to ${bin_path}"
 curl -L $download_url -o ${bin_path}
 chmod +x ${bin_path}
 
-[[ ":$PATH:" != *":${bin_folder}:"* ]] && PATH="/path/to/add:${PATH}"
+echo ""
+echo "$PATH"|grep -q ${install_path} || echo "-> You need to add ${install_path} to your PATH variable. e.g. export PATH=${install_path}:\$PATH;"
+echo ""
 
+echo "$PATH"|grep -q ${install_path} || export PATH=${install_path}:$PATH
 vcli -h
