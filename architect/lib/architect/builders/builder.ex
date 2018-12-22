b5c38a3292423838c4b8889fda8a92c4461dd266
Eddy Lane <naedin@gmail.com>
2018-12-22 03:35:57 +0000

    [architect] Refined API

--- architect/lib/architect/builders/builder.ex
+++ architect/lib/architect/builders/builder.ex
@@ -18,10 +18,14 @@ defmodule Architect.Builders.Builder do
 
   #### Client
 
-  def start_link(%__MODULE__{} = state) do
+  def start_link({%__MODULE__{} = state, name}) do
     Logger.debug("Starting process for builder #{inspect(state)}")
 
-    GenServer.start_link(__MODULE__, state)
+    GenServer.start_link(__MODULE__, state, name: name)
+  end
+
+  def echo(builder_pid) do
+    GenServer.call(builder_pid, :echo)
   end
 
   ### Server
@@ -30,6 +34,20 @@ defmodule Architect.Builders.Builder do
     Logger.info("Running process for builder #{inspect(state)}")
 
     {:ok, state}
+  end
+
+  def handle_call(:state, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call(:echo, _from, state) do
+    Logger.debug("Echo!")
+    {:reply, :echo, state}
+  end
+
+  def handle_cast(:echo, _from, state) do
+    Logger.debug("Echo!")
+    {:reply, state}
   end
 
   def state_ready, do: "ready"
--- architect/lib/architect/builders/builders.ex
+++ architect/lib/architect/builders/builders.ex
@@ -6,6 +6,8 @@ defmodule Architect.Builders do
   @registry Architect.Builders.Registry
   @supervisor Architect.Builders.Supervisor
 
+  @registry_str Atom.to_string(@registry)
+
   ### Client
 
   def start_link(_opts \\ []) do
@@ -15,26 +17,58 @@ defmodule Architect.Builders do
 
   def start_builder(%Builder{id: id} = builder) do
     case Registry.lookup(@registry, id) do
-      [{_, found}] ->
+      [_] ->
         Logger.error(
-          "Attempted to start builder #{inspect(builder)} but id already exists in registry"
+          "Failed to register builder #{inspect(builder)} on #{@registry_str}; id already exists"
         )
 
-        :error
+        {:error, :already_exists}
 
       [] ->
-        {:ok, pid} = DynamicSupervisor.start_child(@supervisor, {Builder, builder})
-        {:ok, _} = Registry.register(@registry, id, pid)
+        name = {:via, Registry, {@registry, id}}
+        {:ok, pid} = DynamicSupervisor.start_child(@supervisor, {Builder, {builder, name}})
 
-        :ok
+        Logger.debug("Registered builder #{inspect(builder)} on #{@registry_str}")
+
+        {:ok, pid}
     end
   end
 
-  def get_builder_state(id) do
-    Registry.dispatch(@registry, id, fn
-      [found] -> true
-      [] -> false
-    end)
+  def stop_builder(id) do
+    case Registry.lookup(@registry, id) do
+      [{pid, _}] ->
+        DynamicSupervisor.terminate_child(@supervisor, pid)
+
+      [] ->
+        Logger.error(
+          "Failed to stop builder #{inspect(id)} on #{@registry_str}; id does not exist"
+        )
+
+        {:error, :not_found}
+    end
+  end
+
+  def call_builder(id, request) do
+    case Registry.lookup(@registry, id) do
+      [{pid, _}] ->
+        try do
+          GenServer.call(pid, request)
+        catch
+          kind, reason ->
+            formatted = Exception.format(kind, reason, __STACKTRACE__)
+
+            Logger.error(
+              "Failed to call builder #{inspect(id)} on #{@registry_str} with #{formatted}"
+            )
+        end
+
+      [] ->
+        Logger.error(
+          "Failed to call builder #{inspect(id)} on #{@registry_str}; id does not exist"
+        )
+
+        {:error, :not_found}
+    end
   end
 
   ### Server
@@ -47,8 +81,14 @@ defmodule Architect.Builders do
 
     state = Supervisor.init(children, strategy: :one_for_one)
 
+    init_builders()
+
     Logger.info("Running #{Atom.to_string(__MODULE__)}")
 
     state
+  end
+
+  defp init_builders() do
+    ### However we get the builders... Still not sure about this bit.
   end
 end
