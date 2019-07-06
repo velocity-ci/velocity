5b250d4602045ad33037d851e94ce31995bf7bae
VJ Patel <vj@vjpatel.me>
2019-07-06 15:03:41 +0100

    add some technical docs and better vuepress settings

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,26 @@
 module.exports = {
-    title: "Velocity CI",
-    themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Guide', link: '/guide/' },
-        { text: 'GitHub', link: 'https://github.com/velocity-ci/velocity' },
-      ],
-      sidebar: [
+  title: "Velocity CI",
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Guide',
+        link: '/guide/'
+      },
+      {
+        text: 'Technical Design',
+        link: '/technical-design/'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/velocity-ci/velocity'
+      },
+    ],
+    sidebar: {
+      '/guide/': [
         '/guide/getting-started',
         '/guide/overview',
         '/guide/examples',
@@ -14,3 +28,4 @@ module.exports = {
       ]
     }
   }
+}
